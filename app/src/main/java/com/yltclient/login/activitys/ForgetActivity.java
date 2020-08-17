28e22a3160a76203a1f55564a00cb38be2db5fb0
unknown <2415109806@qq.com>
2020-08-17 23:17:48 +0800

    忘记密码页面

---
+++ app/src/main/java/com/yltclient/login/activitys/ForgetActivity.java
@@ -0,0 +1,16 @@
+package com.yltclient.login.activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yltclient.R;
+
+public class ForgetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget);
+    }
+}
