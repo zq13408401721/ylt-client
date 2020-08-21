05fda3e3697e6d127deddd951c31f22f21aabd85
longxinpeng901X <lxp860141483@qq.com>
2020-08-21 11:49:00 +0800

    lxf

---
+++ app/src/main/java/com/yltclient/ui/own/activities/ReceivingActivity.java
@@ -0,0 +1,14 @@
+package com.yltclient.ui.own.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceivingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiving);
+    }
+}
